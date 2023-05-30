657530f69d6e021a5a05f30748397049e4909c1f
Matthew Won <matthewwon.gg.no.re@gmail.com>
2023-05-30 14:43:47 -0400

    Attempting to fix animation+image generator

---
+++ scripts/image-track-HTML-generator.js
@@ -0,0 +1,28 @@
+/*
+creating HTML code
+*/
+const imageTrack = [
+    "/images/Homepage_Image_3.JPG",
+    "/images/Homepage_Image_3.JPG",
+    "/images/Homepage_Image_2.JPG",
+    "/images/Homepage_Image_2.JPG",
+    "/images/Homepage_Image_3.JPG",
+    "/images/Homepage_Image_2.JPG",
+    "/images/Homepage_Image_2.JPG",
+    "/images/Homepage_Image_3.JPG",
+    "/images/Homepage_Image_2.JPG",
+    "/images/Homepage_Image_3.JPG",
+    "/images/Homepage_Image_2.JPG"
+]
+
+
+let imageTrackHTML = '';
+
+imageTrack.forEach((image) => {
+    imageTrackHTML += `
+        <img class="image" src="${image}" draggable="false">
+    `;
+    
+});
+
+document.querySelector("js-track").innerHTML = imageTrackHTML;
